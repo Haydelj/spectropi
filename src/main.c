dfaae0d9a2f790c6086b435b44f69641a354eaf4
haydelj <jacbhaydel@gamil.com>
2022-04-08 21:09:27 -0600

    setup pico project

--- src/main.c
+++ src/main.c
@@ -1,22 +1,24 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
 #include "fft.h"
+
+const uint LED_PIN = 25;
 
 int main()
 {
-    complex test[BUFFER_SIZE];
-    for (uint i = 0; i < BUFFER_SIZE; ++i)
-    {
+	stdio_init_all();
 
-        test[i].r = cos(1.0 * 2.0f * PI * i / BUFFER_SIZE);
-        test[i].i = 0.0f;
-    }
-    fft(test);
-
-	float folded[BUFFER_SIZE / 2];
-    fold(test, folded);
-
-	float integrated[16];
-    integrate(folded, integrated);
-    for (uint i = 0; i < 16; ++i) printf("%d: %f\n", i, integrated[i]);
-
-    return 0;
+	gpio_init(LED_PIN);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	
+	while (1)
+	{
+		gpio_put(LED_PIN, 0);
+		sleep_ms(250);
+		gpio_put(LED_PIN, 1);
+		sleep_ms(250);
+	}
 }
